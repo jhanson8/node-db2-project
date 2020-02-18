dcb9f6d6c9dfc45c964016272edc5c8cc42fc62f
Jonathan Hanson <jhanson8@uoregon.edu>
2020-02-18 14:12:35 -0800

    created database

--- migrations/20200218135556_create_cars_table.js
+++ migrations/20200218135556_create_cars_table.js
@@ -1,8 +1,31 @@
+/*jshint esversion: 6 */
 
 exports.up = function(knex) {
-  
+  //do not forget to return
+  return knex.schema.createTable("cars", table => {
+    table.increments(); //primary key
+    table
+      .string("Make", 256)
+      .notNullable()
+      .index(); //makes searching by the name faster
+    table
+      .string("Model", 256)
+      .notNullable()
+      .index();
+    table
+      .integer("VIN")
+      .notNullable()
+      .index();
+    table
+      .integer("Mileage")
+      .notNullable()
+      .index();
+    table.string("Transmission", 128);
+    table.string("Title_Status", 256);
+  });
 };
 
+//how to undo the changes => knex migrate:rollback
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("cars");
 };
