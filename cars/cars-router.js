459fcf607c23b77d53840d1630385f993d616f36
Jonathan Hanson <jhanson8@uoregon.edu>
2020-02-18 14:21:40 -0800

    post and get to database

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -3,13 +3,13 @@
 const express = require("express");
 const knex = require("knex");
 
-// const db = knex({
-//   client: 'sqlite3',
-//   connection: {
-//     filename: './data/produce.db3'
-//   },
-//   useNullAsDefault: true
-// });
+const db = knex({
+  client: "sqlite3",
+  connection: {
+    filename: "./data/car-dealer.db3"
+  },
+  useNullAsDefault: true
+});
 
 const router = express.Router();
 
